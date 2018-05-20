67e6b95ad0a47a40aa9d2e328259653da3d8c80d
Seven Lju <dna2gle@gmail.com>
2018-05-20 13:15:50 +0800

    [modules] mxnet.js bugfix
    - sed mxnet_predict.js for adding full path to
      make sure it can find mem file correctly

--- modules/mxnet/prepare_mxnet.sh
+++ modules/mxnet/prepare_mxnet.sh
@@ -3,3 +3,8 @@
 git clone https://github.com/dmlc/mxnet.js/
 cd mxnet.js
 cp -r libmxnet_predict.js libmxnet_predict.js.mem mxnet_predict.js model ../
+
+# patch file to make sure libmxnet_predict.js.mem can be found
+cd ..
+# macosx sed -i '' 's...'
+sed -i 's|"libmxnet_predict.js.mem"|__dirname+"/libmxnet_predict.js.mem"|g' libmxnet_predict.js
